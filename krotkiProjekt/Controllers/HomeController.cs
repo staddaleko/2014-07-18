247d3f53f1bf21420fdb88ec16d4d2a1cd8f1aab
staddaleko <staddaleko@gmail.com>
2014-07-18 11:47:06 +0200

    1146 jeszcze nie do końca działa

--- krotkiProjekt/Controllers/HomeController.cs
+++ krotkiProjekt/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using krotkiProjekt;
+using krotkiProjekt.Models;
 
 namespace krotkiProjekt.Controllers
 {
@@ -12,9 +12,29 @@ namespace krotkiProjekt.Controllers
         
         public ActionResult Index()
         {
-            var db = new MyDB();
-            return View("Index"), db.Autorzy;
+            var db = new MyDb();
+            return View("Index", db.Autorzy.ToList());
+        }
+        public ActionResult Partial_Detale_A(int id_a)
+        {
+            var db = new MyDb();
+            var ksiazki = db.Autorzy.Join(db.Asocjacja, 
+                    aut => aut.Id_A, 
+                    asoc => asoc.Id_A, 
+                    (aut, asoc) => new { aut.Id_A, asoc.Id, asoc.Id_K })
+                .Join(db.Ksiazki, 
+                    tmp1 => tmp1.Id_K, 
+                    k => k.Id_K, 
+                    (tmp1, k) => new { tmp1.Id_A, tmp1.Id_K, k.Tytul, k.Opis, k.Data_wydania})
+                .Where(c=>c.Id_A == id_a)
+                .ToList();// tu jest wazna kolejność
+            return PartialView("Partial_Detale_A", ksiazki);
         }
 
+        public ActionResult Detale_A(int id_a)
+        {
+            var db = new MyDb();
+            return View("Detale", db.Autorzy.Find(id_a));
+        }
     }
 }
--- krotkiProjekt/Models/KataogController.cs
+++ krotkiProjekt/Models/KataogController.cs
@@ -39,4 +39,18 @@ namespace krotkiProjekt.Models
         public DbSet<Ksiazka> Ksiazki { get; set; }
         public DbSet<Ksiazka_Autor> Asocjacja { get; set; }
     }
+    public class Temp1
+    { //ważna jest kolejność w porównaniu do kontrolera 'Home' i var 'Ksiazki' wyrażenie lambda
+        public int Id_A { get; set; }
+        public int Id { get; set; }
+        public int Id_K { get; set; }
+    }
+    public class Temp2
+    {
+        public int Id_A { get; set; }
+        public int Id_K { get; set; }
+        public string Tytul { get; set; }
+        public string Opis { get; set; } //to można usunąć, ale zostawiamy ;)
+        public DateTime Data_wydania { get; set; }
+    }
 }
